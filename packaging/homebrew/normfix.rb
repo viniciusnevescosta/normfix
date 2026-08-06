051e55a5b74235804c91deeb0cdb2695deccc3f7
viniciusnevescosta <vinicius.nevesco@gmail.com>
2026-08-06 09:06:54 -0300

    chore(release): update the Homebrew formula to 0.4.0-beta.4

--- packaging/homebrew/normfix.rb
+++ packaging/homebrew/normfix.rb
@@ -6,28 +6,28 @@
 class Normfix < Formula
   desc "Safe automatic fixes and clear diagnostics for the 42 Norm"
   homepage "https://normfix.vercel.app"
-  version "0.4.0-beta.3"
+  version "0.4.0-beta.4"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.3/normfix-aarch64-macos.tar.gz"
-      sha256 "a987d83f92ec663fc88c6bc5cf97773a3eb415237c71b239d20add2895994674"
+      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.4/normfix-aarch64-macos.tar.gz"
+      sha256 "74dde753caa99df7f6d754d0dabb3e1a140929a0cb07d867dc06b8770f4c78c7"
     end
     on_intel do
-      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.3/normfix-x86_64-macos.tar.gz"
-      sha256 "4b8f6a8d46f2b8e1eab6e755fb3aca670671b49bf7efa1bbca62a67303ee67f4"
+      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.4/normfix-x86_64-macos.tar.gz"
+      sha256 "788ead69948a8deb8bfa451b618dcc6e441e1f4261e4cc4faf48bf522f464214"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.3/normfix-aarch64-linux-gnu.tar.gz"
-      sha256 "4cd059d941919485fe1d689ba373a2f10f4c5542d39dc940073d600957fae704"
+      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.4/normfix-aarch64-linux-gnu.tar.gz"
+      sha256 "9273cd555ddffe7b8fb6efaa4c418556bd0ec8c671029b835aa6929a0d6c0131"
     end
     on_intel do
-      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.3/normfix-x86_64-linux-gnu.tar.gz"
-      sha256 "f96907cd47e04559663bd691d7cd10f09ab3ffbbe191413ea5a92ebcaab77101"
+      url "https://github.com/viniciusnevescosta/normfix/releases/download/v0.4.0-beta.4/normfix-x86_64-linux-gnu.tar.gz"
+      sha256 "12722cb000e2e24a7707b3086898f6f4847e6d6ea2cdbfc8238d932787fa5cb8"
     end
   end
 
